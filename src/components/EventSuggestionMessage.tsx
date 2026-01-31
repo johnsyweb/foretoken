5b4cd2b5a78ed5b74c3f887ecc42f6eca99e3582
Pete Johns <paj+github@johnsy.com>
2026-01-31 11:35:12 +1100

    Refactor AboutBox and message logic; move all text and props to message files; clean up App.tsx

---
+++ src/components/EventSuggestionMessage.tsx
@@ -0,0 +1,40 @@
+export const eventSuggestionMessageProps = {
+  ariaLabel: "Show event suggestion",
+  toggleText: "Thinking bigger? Tap this when you are finished scanning!",
+  content: (
+    <>
+      <h2>Help your local parkrun community grow!</h2>
+      <p>
+        This parkrun event is thriving! When we start to run out of finish
+        tokens, it is time to consider starting a new parkrun event nearby. This
+        helps keep courses safe and fun for everyone, and gives more people the
+        chance to enjoy a free, fun and friendly 5km each week.
+      </p>
+      <p>
+        Being an event director is a truly rewarding way to support your
+        community. If you are interested, check out these resources:
+      </p>
+      <ul>
+        <li>
+          <a
+            href="https://www.parkrun.com/about/join-us/start-your-own-event/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            How to start a new parkrun event
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://volunteer.parkrun.com/hc/en-us/articles/16857103665170-2-2-Event-Location-and-Course-Design-Guidance#h_01HWSR6K8HMSN37WCJ13KB7YY1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Event location and course design guidance
+          </a>
+        </li>
+      </ul>
+      <p>Have a chat with today's event team if you would like to know more!</p>
+    </>
+  ),
+};
